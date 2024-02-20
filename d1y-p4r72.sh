620b55e76a2b18827e35734da684a04cac8da32f
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-02-19 21:47:28 -0800

    Update d1y-p4r72.sh

--- d1y-p4r72.sh
+++ d1y-p4r72.sh
@@ -13,6 +13,15 @@ sed -i 's/root:::0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:0:0:99999:
 # Change language=auto to zh_cn
 # sed -i 's/lang="auto"/lang="zh_cn"/g' package/emortal/default-settings/files/99-default-settings
 
+#. Modify Hostname
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='LedeWrt'' package/lean/default-settings/files/zzz-default-settings
+
+#. Modify builder
+sed -i "s/LEDE /0xACE7 build $(TZ=UTC-3 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+#. Change luci list name
+sed -i 's/"Turbo ACC 网络加速"/"网络加速"/g' feeds/luci/applications/luci-app-turboacc/po/zh-cn/turboacc.po
+
 # Change ash to bash
 sed -i 's/ash/bash/g' package/base-files/files/etc/passwd
 
