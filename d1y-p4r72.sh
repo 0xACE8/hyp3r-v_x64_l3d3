ca76ecbdb2f088c30df4f4144ef38e41a057f47a
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-02-19 21:41:39 -0800

    Update d1y-p4r72.sh

--- d1y-p4r72.sh
+++ d1y-p4r72.sh
@@ -23,18 +23,18 @@ sed -i 's/ash/bash/g' package/base-files/files/etc/passwd
 #sed -i 's/ntp.tencent.com/ntp.tuna.tsinghua.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 
 # luci theme argon update
-sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/ace8/luci-app-argon-config/po/zh_Hans/argon-config.po
+sed -i 's/"Argone 主题设置"/"主题设置"/g' feeds/ace8/luci-app-argone-config/po/zh-cn/argone-config.po
 
-rm -rf feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-wget --no-check-certificate -O feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg "https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/bg1.jpg"
+rm -rf feeds/ace8/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg
+wget --no-check-certificate -O feeds/ace8/luci-theme-argone/htdocs/luci-static/argone/img/bg1.jpg "https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_l3d3/main/bg1.jpg"
 
-sed -i 's/5e72e4/1c78ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-sed -i 's/5e72e4/1c78ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
-sed -i 's/483d8b/1c78ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
-sed -i 's/483d8b/1c78ff/g' feeds/ace8/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
+sed -i 's/5e72e4/1c78ff/g' feeds/ace8/luci-theme-argone/htdocs/luci-static/argone/css/cascade.css
+sed -i 's/5e72e4/1c78ff/g' feeds/ace8/luci-theme-argone/htdocs/luci-static/argone/css/dark.css
+sed -i 's/483d8b/1c78ff/g' feeds/ace8/luci-theme-argone/htdocs/luci-static/argone/css/cascade.css
+sed -i 's/483d8b/1c78ff/g' feeds/ace8/luci-theme-argone/htdocs/luci-static/argone/css/dark.css
 
 # ttyd menu name
-sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+# sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 
 # dnscrypt-proxy start
 sed -i 's/START=18/START=99/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
@@ -56,10 +56,10 @@ echo 'net.core.netdev_max_backlog=2048' >>package/base-files/files/etc/sysctl.d/
 # wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/99-default-settings-chinese.sh -O package/emortal/default-settings/files/99-default-settings-chinese.sh
 
 # add init settings
-# wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/99-init-settings -O package/base-files/files/etc/uci-defaults/99-init-settings
+wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_l3d3/main/99-init-settings -O package/base-files/files/etc/uci-defaults/99-init-settings
 
 # Change to my banner
 sudo rm package/base-files/files/etc/banner
-wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/banner -O package/base-files/files/etc/banner
+wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_l3d3/main/banner -O package/base-files/files/etc/banner
 
 echo "diy-part2.sh is done."
